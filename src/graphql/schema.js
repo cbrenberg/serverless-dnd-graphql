e7ae429834e6b9412c684083b52e9819b93ae822
Christopher Brenberg <cbrenberg@gmail.com>
2019-03-17 20:36:29 -0500

    create GraphQL schema file

---
+++ src/graphql/schema.js
@@ -0,0 +1,30 @@
+'use strict';
+
+//import GraphQL
+const {
+  GraphQLSchema,
+  GraphQLObjectType
+} = require('graphql');
+
+//import project queries
+const SpellQuery = require('../graphql/queries/Spell');
+const SpellsQuery = require('../graphql/queries/Spells');
+const ClassQuery = require('../graphql/queries/Class');
+const ClassesQuery = require('../graphql/queries/Classes');
+
+
+const RootQuery = new GraphQLObjectType({
+  name: 'Root Query',
+  description: 'This is the default query provided by this application',
+
+  fields: {
+    spell: SpellQuery.index(),
+    spells: SpellsQuery.index(),
+    class: ClassQuery.index(),
+    classes: ClassesQuery.index()
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+})
